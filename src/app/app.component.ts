34658582c17f15589a24eeab66681bceacf9fe0f
Sigurd P. von A. Vilstrup <32160689+SigurdVilstrup@users.noreply.github.com>
2023-02-09 13:48:04 +0100

    adds bat and tønde

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'slaa-katten-af-toenden';
+
+  hitClicked() {
+    alert('Du trykkede på SLÅ!');
+  }
 }
