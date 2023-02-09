6caa849c52b3f2cf0b856c66d7451eb3826880f7
Sigurd P. von A. Vilstrup <32160689+SigurdVilstrup@users.noreply.github.com>
2023-02-09 15:22:29 +0100

    adds popup component and hit action

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuestionPopupComponent } from './question-popup/question-popup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionPopupComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/question-popup/question-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question-popup',
+  templateUrl: './question-popup.component.html',
+  styleUrls: ['./question-popup.component.scss']
+})
+export class QuestionPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
